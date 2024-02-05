00d03f379f0cc22baf663a08efcbb2db67660b13
Nicole <124882220+nikkichep@users.noreply.github.com>
2024-02-05 11:58:35 +0300

    linking animation to app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
